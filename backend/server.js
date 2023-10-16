fe55277dc463478334ca80f5204ff586e74f890f
Kannan K Pillai <kpillaikannan62@gmail.com>
2023-10-16 16:40:14 +0530

    fixed es6 errors

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,16 @@ import path from 'path';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-dotenv.config();
 import cookieParser from "cookie-parser";
-const port = process.env.PORT || 5000;
 import cors from 'cors';
 import userRoutes from'./routes/userRoutes.js'
 import adminRoutes from './routes/adminRoutes.js'
 import ownerRoutes from './routes/ownerRoutes.js'
 
+
+
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
