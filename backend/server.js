7e92be3ca21fdc86ded82edc5b58d94f34533b33
Kannan K Pillai <kpillaikannan62@gmail.com>
2023-10-17 17:27:46 +0530

    hhhh

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
-import path from 'path';
 import dotenv from 'dotenv';
+import path from 'path';
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
